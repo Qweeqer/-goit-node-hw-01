5f9043558c0ef93af2fb956c3b4776a91c502e98
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-24 17:49:17 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -5,13 +5,26 @@ const { nanoid } = require("nanoid");
 // Розкоментуйте і запиши значення
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
+const Error = {
+  INVALID_ID: "No contact with such ID.",
+};
+
+const write = async (payload) => {
+  await fs.writeFile(contactsPath, JSON.stringify(payload, null, 2));
+};
+
 // TODO: задокументувати кожну функцію
 function listContacts() {
   // ...твій код
+  const contacts = fs.readFile(contactsPath);
+  return JSON.parse(contacts);
 }
 
 function getContactById(contactId) {
   // ...твій код
+  const contacts = listContacts();
+  const goal = contacts.find((contact) => contact.id === contactId);
+  return goal || Error.INVALID_ID;
 }
 
 function removeContact(contactId) {
@@ -20,4 +33,16 @@ function removeContact(contactId) {
 
 function addContact(name, email, phone) {
   // ...твій код
+  const contacts = listContacts();
+  const payload = { id: nanoid(), name, email, phone };
+  contacts.push(payload);
+  write(contacts);
+  return payload;
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
